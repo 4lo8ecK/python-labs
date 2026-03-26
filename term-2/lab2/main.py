58d5455bdb8a98711048cbb909e36118035315e8
4lo8ecK <4lo8ecK@vk.com>
2026-03-26 10:02:28 +0300

    Исправлена нумерация лабораторных работ

--- term-2/lab2/main.py
+++ term-2/lab2/main.py
@@ -1,71 +1,80 @@
-class Movie:
-    def __init__(self, title: str = None, director: str = None, release_year: int = None) -> None:
-        if title == None or director == None or release_year == None:
-            raise ValueError("Все аргументы должны быть переданы в конструктор")
-        self.__ttl = title
-        self.__drctr = director
-        self.__rls_year = release_year
+
+class Rectangle:
+    def __init__(self,
+        x: int|float = 0, y: int|float = 0,
+        width: int|float = 1, height: int|float = 1
+        ) -> None:
+        
+        """
+        Подразумевается, что по умолчанию прямоугольник единичный
+        и находится в координатах (0;0)
+        Координатой прямоугольника является левый нижний угол
+        """
+        if width < 0 or height < 0: raise ValueError("Длина стороны должна быть больше нуля")
+
+        self.x = x
+        self.y = y
     
+        self.width = width
+        self.height = height
+
+        self.x1 = self.x + self.width
+        self.y1 = self.y + self.height
+
     def __str__(self) -> str:
-        return f"\"{self.__ttl}\", {self.__drctr}, {self.__rls_year}"
+        return f"Координаты:\t({self.x}, {self.y})\nРазмеры:\t({self.width}, {self.height})"
 
-    def get_title(self) -> str:
-        return self.__ttl
+    def mov(self, new_x: int|float = None, new_y: int|float = None) -> None:
+        if new_x != None: self.x = new_x
+        if new_y != None: self.y = new_y
+        
     
-    def get_director(self) -> str:
-        return self.__drctr
-    
-    def get_release_year(self) -> int:
-        return self.__rls_year
+    def resize(self, new_x: int|float = None, new_y: int|float = None) -> None:
+        if new_x != None: self.width = new_x
+        if new_y != None: self.height = new_y
 
-class MovieCollection:
-    def __init__(self) -> None:
-        self.__lib: list = []
-    
-    def __str__(self) -> str:
-        res = ""
-        for i in self.__lib:
-            res += str(i) + '\n'
-        return f"Полный список фильмов:\n{res}"
+    @staticmethod
+    def do_intersect(a: Rectangle, b: Rectangle) -> bool:
+        return (min(a.x1, b.x1) <= max(a.x1, b.x1)) and (min(a.y1, b.y1) <= max(a.y1, b.y1))
 
-    def new_movie(self, mov: Movie = None) -> None:
-        if (mov != None) or not(mov in self.__lib):
-            self.__lib += [mov]
-    
-    def remove_movie(self, mov: Movie = None) -> None:
-        if (mov != None) or (mov in self.__lib):
-            self.__lib.remove(mov)
-    
-    def find_movie_by_director(self, director: str = None) -> Movie:
-        if director != None:
-            for mov in self.__lib:
-                if mov.get_director() == director:
-                    return mov
+        
+    @staticmethod
+    def new_by_two(a: Rectangle, b: Rectangle) -> Rectangle:
 
-    def sort_movies_by_year(self) -> None:
-        self.__lib.sort(key = lambda mov: mov.get_release_year())
+        new_x = min(a.x, b.x)
+        new_y = min(a.y, b.y)
+
+        new_width = max(a.x1, b.x1) - min(a.x, b.x)
+        new_height = max(a.y1, b.y1) - min(a.y, b.y)
+
+        return Rectangle(width=new_width, height=new_height, x=new_x, y=new_y)
+
+    @staticmethod
+    def intersection(a: Rectangle, b: Rectangle) -> Rectangle:
+        if Rectangle.do_intersect(a,b):
+            wid = max(a.width, b.width) - min(a.width, b.width)
+            hght = max(a.height, b.height) - min(a.height, a.width)
+            
+            x = max(a.x, b.x) - min(a.x, b.x)
+            y = max(a.y, b.y) - min(a.y, b.y)
+
+            return Rectangle(x=x, y=y, width=wid, height=hght)
+        else: return None
 
 # int main() :)
 if __name__ == "__main__":
 
-    mc = MovieCollection()
+    r = Rectangle()
+    # r1 = Rectangle(width=2, height=5, x=1, y=3)
+    r2 = Rectangle(2, 5, 2, 3)
+    
+    print(f"r:\n{r}\n")
+    # print(f"r1:\n{r1}\n")
+    print(f"r2:\n{r2}\n")
 
-    m1 = Movie("Аватар", "Дж. Кэмерон", 2009)
-    m2 = Movie("Звёздные войны: новая надежда", "Дж. Лукас", 1977)
-    m3 = Movie("Звёздные войны: пробуждение силы", "Дж. Дж. Абрамс", 2015)
+    print("Пересечение: ", Rectangle.do_intersect(r, r2))
+    # print(Rectangle.new_by_two(r, r2))
+    print(Rectangle.intersection(r, r2))
 
-    mc.new_movie(m1)
-    mc.new_movie(m3)
-    mc.new_movie(m2)
+    pass
 
-    print(mc)
-
-    print(mc.find_movie_by_director(director="Дж. Кэмерон"), end='\n\n')
-
-    mc.sort_movies_by_year()
-
-    print(mc)
-
-    mc.remove_movie(m1)
-
-    print(mc)
--- term-2/lab3/main.py
+++ term-2/lab3/main.py
@@ -1,80 +1,154 @@
+import colors as col
 
-class Rectangle:
-    def __init__(self,
-        x: int|float = 0, y: int|float = 0,
-        width: int|float = 1, height: int|float = 1
-        ) -> None:
+DBG = True
+
+# константы
+CLS = '\x1bc'
+
+SUBJ    = 'subj'
+TIME    = 'time'
+AUD     = 'aud'
+
+class Time:
+    def __init__(self, h: int = 0, m: int = 0, s: int = 0) -> None:
+        if not(0 <= s < 60 or 0 <= m < 60):
+            raise ValueError("Секунды или минуты должны быть в промежутке от 0 до 59")
+        if not(0<= h <= 24):
+            raise ValueError("Часы должны быть в промежутке от 0 до 24")
+
+        self.hours = h
+        self.minutes = m
+        self.seconds = s
         
-        """
-        Подразумевается, что по умолчанию прямоугольник единичный
-        и находится в координатах (0;0)
-        Координатой прямоугольника является левый нижний угол
-        """
-        if width < 0 or height < 0: raise ValueError("Длина стороны должна быть больше нуля")
+        if DBG: print(col.style(col.dim, col.yellow), "вызван конструктор класса Time", col.style(0), sep='')
 
-        self.x = x
-        self.y = y
     
-        self.width = width
-        self.height = height
+    def __del__(self) -> None:
+        del self.hours
+        del self.minutes
+        del self.seconds
+        if DBG: print(col.style(col.dim, col.yellow), "вызван деструктор класса Time", col.style(0), sep='')
+    
+    def __str__(self) -> str:
+        return f"Time({self.hours}:{self.minutes}:{self.seconds})"
+    
+    # "приватные" методы
+    def __add_n_min(self, n: int = 0) -> None:
+        summ = self.minutes + n
+        hours_to_add = summ // 60
+        delta = summ - (60 * hours_to_add)
+        self.hours += hours_to_add
+        self.minutes = delta
 
-        self.x1 = self.x + self.width
-        self.y1 = self.y + self.height
+    @staticmethod
+    def _get_time_added_n(time: Time = None, n: int = 0) -> Time:
+        if Time == None or not isinstance(time, Time):
+            raise ValueError("аргумент time должен иметь значение")
+        mins_summ = time.minutes + n
+        hours_to_add = mins_summ // 60
+        delta = mins_summ - (60 * hours_to_add)
+        return Time(time.hours + hours_to_add, delta, time.seconds)
+
+    # методы задания
+    def add_100_min(self) -> None:
+        self.__add_n_min(100)
+
+    def minutes_to_midnight(self) -> int:
+        mins_in_day = 24 * 60
+        minutes = self.hours * 60 + self.minutes
+        return mins_in_day - minutes
+
+class Timetable(Time):
+    LESSON_LEN = 80 # min
+
+    def __init__(self, h: int = 0, m: int = 0, s: int = 0, lessons: list[dict] = None) -> None:
+        super().__init__(h, m, s)
+        if lessons == None or not isinstance(lessons, list):
+            raise ValueError("некорректные данные переданы как расписание")
+        for i in lessons:
+            keys_lst = i.keys()
+            if not(SUBJ in keys_lst) or not(TIME in keys_lst) or not(AUD in keys_lst):
+                lessons.remove(i)
+                if DBG: print(col.style(col.dim, col.red), 'DELETE: ', col.style(col.dim), i, col.style(col.dim, col.italic, col.yellow), ' has been removed', col.style(0), sep='')
+        if DBG: print(col.style(col.dim), 'Итоговый список:\n', lessons, col.style(0), sep='')
+        self.lessons = lessons
+        if DBG: print(col.style(col.dim, col.yellow), "вызван конструктор класса Timetable", col.style(0), sep='')
+
+
+    def subject_start(self) -> str:
+        for i in self.lessons:
+            # l_t <- Lesson_Time
+            # le_t <- LessonEnd_Time
+            l_t = i[TIME] 
+            le_t = Time._get_time_added_n(i[TIME], self.LESSON_LEN) 
+            if ((l_t.hours <= self.hours <= le_t.hours) and
+                (l_t.minutes <= self.minutes <= le_t.minutes) and
+                (l_t.seconds <= self.seconds <= le_t.seconds)):
+                return i[SUBJ]
+        return "Занятий нет"
 
     def __str__(self) -> str:
-        return f"Координаты:\t({self.x}, {self.y})\nРазмеры:\t({self.width}, {self.height})"
-
-    def mov(self, new_x: int|float = None, new_y: int|float = None) -> None:
-        if new_x != None: self.x = new_x
-        if new_y != None: self.y = new_y
-        
+        return f"Timetable: time({self.hours}, {self.minutes}, {self.seconds}), subject: {self.subject_start()}"
+        pass
     
-    def resize(self, new_x: int|float = None, new_y: int|float = None) -> None:
-        if new_x != None: self.width = new_x
-        if new_y != None: self.height = new_y
-
-    @staticmethod
-    def do_intersect(a: Rectangle, b: Rectangle) -> bool:
-        return (min(a.x1, b.x1) <= max(a.x1, b.x1)) and (min(a.y1, b.y1) <= max(a.y1, b.y1))
-
-        
-    @staticmethod
-    def new_by_two(a: Rectangle, b: Rectangle) -> Rectangle:
-
-        new_x = min(a.x, b.x)
-        new_y = min(a.y, b.y)
-
-        new_width = max(a.x1, b.x1) - min(a.x, b.x)
-        new_height = max(a.y1, b.y1) - min(a.y, b.y)
-
-        return Rectangle(width=new_width, height=new_height, x=new_x, y=new_y)
-
-    @staticmethod
-    def intersection(a: Rectangle, b: Rectangle) -> Rectangle:
-        if Rectangle.do_intersect(a,b):
-            wid = max(a.width, b.width) - min(a.width, b.width)
-            hght = max(a.height, b.height) - min(a.height, a.width)
-            
-            x = max(a.x, b.x) - min(a.x, b.x)
-            y = max(a.y, b.y) - min(a.y, b.y)
-
-            return Rectangle(x=x, y=y, width=wid, height=hght)
-        else: return None
-
-# int main() :)
-if __name__ == "__main__":
-
-    r = Rectangle()
-    # r1 = Rectangle(width=2, height=5, x=1, y=3)
-    r2 = Rectangle(2, 5, 2, 3)
-    
-    print(f"r:\n{r}\n")
-    # print(f"r1:\n{r1}\n")
-    print(f"r2:\n{r2}\n")
-
-    print("Пересечение: ", Rectangle.do_intersect(r, r2))
-    # print(Rectangle.new_by_two(r, r2))
-    print(Rectangle.intersection(r, r2))
-
+    def __del__(self) -> None:
+        super().__del__()
+        del self.lessons
+        if DBG: print(col.style(col.dim, col.yellow), "вызван деструктор класса Timetable", col.style(0), sep='')
     pass
 
+if __name__ == "__main__":
+
+    print(CLS)
+    cmd = input('Показывать отладочную информацию? (y/N): ')
+    if cmd == 'y' or cmd == 'Y' or cmd == 'Н' or cmd == 'н':
+        DBG = True
+    else:
+        DBG = False
+    print(CLS)
+
+    lessons_list = [
+        {SUBJ: 'Линейная алгебра',                    TIME: Time(8, 20),    AUD: '2-06'},
+        {SUBJ: 'Программирование в Python',           TIME: Time(9, 50),    AUD: 'I-321'},
+        {SUBJ: 'Программирование в Python',           TIME: Time(9, 50),    'auditory': 'I-321'},
+        {SUBJ: 'Иностранный язык',                    TIME: Time(11, 40),   AUD: 'I-314'},
+        {SUBJ: 'Введение в искусственный интеллект',  TIME: Time(13, 30),   AUD: 'I-306'}]
+
+    print('\n\t', col.style(1, 21, 33), '=== Проверки для Time ===', col.style(0), '\n')
+    
+    print(col.style(1), 'Проверка 1:', col.style(0), sep='')
+    a = Time()
+    print(col.style(32), 'Инициализация Time без аргументов:\t',col.style(), a, sep='')
+    a.add_100_min()
+    print(col.style(32), 'Добавление 100 минут:\t\t\t',col.style(), a, sep='')
+    print(col.style(32), 'Количество минут до полуночи:\t\t',col.style(), a.minutes_to_midnight(), sep='')
+
+    print()
+
+    print(col.style(1), 'Проверка 2:', col.style(0), sep='')
+    b = Time(15, 8, 68)
+    print(col.style(32), 'Инициализация Time с (15, 8, 68):\t',col.style(), b, sep='')
+    b.add_100_min()
+    print(col.style(32), 'Добавление 100 минут:\t\t\t',col.style(), b, sep='')
+    print(col.style(32), 'Количество минут до полуночи:\t\t',col.style(), b.minutes_to_midnight(), sep='')
+    
+    print()
+    del a
+    del b
+
+    print('\n\t', col.style(1, 21, 33), '=== Проверки для Timetable ===', col.style(0), '\n')
+    print(col.style(1), 'Проверка 1:', col.style(0), sep='')
+
+    a = Timetable(lessons=lessons_list)
+    print(col.style(32), 'Инициализация Timetable без аргументов:\t', col.style(), a, sep='')
+    print(col.style(32), 'Дисциплина, в указанное время:\t\t', col.style(), a.subject_start(), sep='')
+
+    print()
+
+    print(col.style(1), 'Проверка 2:', col.style(0), sep='')
+
+    b = Timetable(8, 30, 0, lessons_list)
+    print(col.style(32), 'Инициализация Timetable (8, 30, 0):\t', col.style(), b, sep='')
+    print(col.style(32), 'Дисциплина, в указанное время:\t\t', col.style(), b.subject_start(), sep='')
+
+    print()
