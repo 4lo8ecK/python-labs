442685924d82ff69644f92c4c892055099f21001
4lo8ecK <4lo8ecK@vk.com>
2026-04-16 10:59:30 +0300

    начата работа над рисунком

--- term-2/lab6/main.py
+++ term-2/lab6/main.py
@@ -1,4 +1,24 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
-if __name__ == "__main__":
-    pass
+IMG_WIDTH = 854
+IMG_HEIGHT = 480
+
+# Создание неба
+img = Image.new("RGB", (IMG_WIDTH, IMG_HEIGHT), (215, 235, 250))
+
+draw = ImageDraw.Draw(img)
+
+
+HORIZON = 200
+# горы
+m1 = [(-50, HORIZON), (70, 150), (240, HORIZON)]
+m2 = [(200, HORIZON), (360, 130), (523, HORIZON)]
+m3 = [(360, HORIZON), (600, 175), (780, HORIZON)]
+m4 = [(200, HORIZON), (360, 130), (523, HORIZON)]
+
+draw.polygon(m1, fill="white")
+draw.polygon(m2, fill="white")
+draw.polygon(m3, fill="white")
+
+
+img.save('./tst.png', 'PNG')
